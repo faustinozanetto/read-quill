b4e5203904112d90cc3972943d297d7df8d15b19
Faustino Zanetto <zanettofaustino@gmail.com>
2024-01-18 17:29:59 -0300

    feat(core): updated tailwind cofing

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,6 @@
+const sharedConfig = require('@read-quill/tailwind-config');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [sharedConfig],
+};
--- apps/web/tailwind.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'tailwindcss';
-import sharedConfig from '@read-quill/tailwind-config';
-
-const config: Pick<Config, 'presets'> = {
-  presets: [sharedConfig],
-};
-
-export default config;
