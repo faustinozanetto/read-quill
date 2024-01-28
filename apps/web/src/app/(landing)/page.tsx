3cd985a7c6269ffc8e4bf3175e4ce33e8b019fa5
Faustino Zanetto <zanettofaustino@gmail.com>
2024-01-28 20:31:58 -0300

    fix(web): fix landing data fetch

--- apps/web/src/app/(landing)/page.tsx
+++ apps/web/src/app/(landing)/page.tsx
@@ -8,7 +8,10 @@ import HomeFeatures from '@modules/landing/components/sections/features/home-fea
 export default async function Page(): Promise<React.JSX.Element> {
   const url = new URL('/api/landing/stats', __URL__);
   const response = await fetch(url, { method: 'GET' });
-  const data: LandingStatsGetResponse = await response.json();
+  let data: LandingStatsGetResponse = { activeUsers: 0, annotationsCreated: 0, booksRegistered: 0, pagesRead: 0 };
+  if (response.ok) {
+    data = await response.json();
+  }
 
   return (
     <div className="flex w-full flex-col items-center justify-center">
