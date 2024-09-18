e1eb41447fb86dbcfe4c31bfaa5b8a6079aacfc9
Faustino Zanetto <zanettofaustino@gmail.com>
2024-09-18 20:54:27 -0300

    feat(user-avatar): added fallback to initials when no avatar found

--- apps/web/src/modules/users/components/profile/details/avatar/user-profile-avatar.tsx
+++ apps/web/src/modules/users/components/profile/details/avatar/user-profile-avatar.tsx
@@ -7,30 +7,42 @@ import UserProfileAvatarUpdate from './user-profile-avatar-update';
 import { useUserContext } from '@modules/users/hooks/use-user-context';
 
 interface UserProfileAvatarProps {
-  avatar: DBImage;
+  name: string;
+  avatar: DBImage | null;
 }
 
 const UserProfileAvatar: React.FC<UserProfileAvatarProps> = (props) => {
-  const { avatar } = props;
+  const { name, avatar } = props;
 
   const { user: authUser } = useAuthContext();
   const { user } = useUserContext((s) => s);
 
   const isProfileOwner = Boolean(authUser && user.email === authUser.email);
-  const url = getImagePublicUrl('UserAvatars', avatar.path);
+  const url = avatar ? getImagePublicUrl('UserAvatars', avatar.path) : null;
+
+  const userInitials = name
+    .split(' ')
+    .map((char) => char.charAt(0).toUpperCase())
+    .join('');
 
   return (
     <div className="relative">
-      <Image
-        src={url}
-        alt="User Avatar"
-        aria-label="User Avatar"
-        className="h-32 w-full rounded-lg object-cover object-center md:h-36 md:w-36"
-        draggable={false}
-        priority
-        height={512}
-        width={512}
-      />
+      {url ? (
+        <Image
+          src={url}
+          alt="User Avatar"
+          aria-label="User Avatar"
+          className="h-32 w-full rounded-lg object-cover object-center md:h-36 md:w-36"
+          draggable={false}
+          priority
+          height={512}
+          width={512}
+        />
+      ) : (
+        <div className="h-32 w-full rounded-lg object-cover object-center md:h-36 md:w-36 shadow border text-4xl font-bold flex items-center justify-center aspect-square text-primary">
+          {userInitials}
+        </div>
+      )}
       {isProfileOwner && <UserProfileAvatarUpdate />}
     </div>
   );
